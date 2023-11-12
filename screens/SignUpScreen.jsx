1686294768b8f00810a394e002d830ee7655f172
Admin <cvbvitmitm45@gmail.com>
2023-11-13 00:15:43 +0500

    pages refactored

--- screens/SignUpScreen.jsx
+++ screens/SignUpScreen.jsx
@@ -1,8 +1,6 @@
-import React, { useCallback, useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import { View, TouchableHighlight, StyleSheet } from 'react-native';
 import styled from 'styled-components/native';
-import * as SplashScreen from 'expo-splash-screen';
-import * as Font from 'expo-font';
 
 export const Title = styled.Text`
   font-size: 28px;
@@ -59,62 +57,11 @@ export const Img = styled.Image`
   align-items: center;
 `;
 
-export const SignUpScreen = ({ navigation }) => {
+export const SignUpScreen = ({ onLayoutRootView, touchProps, navigation }) => {
   const [name, setName] = useState('');
   const [surName, setSurName] = useState('');
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
-
-  const [appIsReady, setAppIsReady] = useState(false);
-
-  const [isPress, setIsPress] = useState(false);
-
-  const touchProps = {
-    activeOpacity: 1,
-    underlayColor: 'grey', // <-- "backgroundColor" will be always overwritten by "underlayColor"
-    style: isPress ? styles.btnPress : styles.btnNormal, // <-- but you can still apply other style changes
-    onHideUnderlay: () => setIsPress(false),
-    onShowUnderlay: () => setIsPress(true),
-    onPress: () => {}, // <-- "onPress" is apparently required
-  };
-
-  useEffect(() => {
-    async function prepare() {
-      try {
-        // Pre-load fonts, make any API calls you need to do here
-        await Font.loadAsync({
-          'roboto-regular': require('../assets/fonts/Roboto-Regular.ttf'),
-          'roboto-bold': require('../assets/fonts/Roboto-Bold.ttf'),
-          'ubuntu-regular': require('../assets/fonts/Ubuntu-Regular.ttf'),
-        });
-        // Artificially delay for two seconds to simulate a slow loading
-        // experience. Please remove this if you copy and paste the code!
-        await new Promise((resolve) => setTimeout(resolve, 2000));
-      } catch (e) {
-        console.warn(e);
-      } finally {
-        // Tell the application to render
-        setAppIsReady(true);
-      }
-    }
-
-    prepare();
-  }, []);
-
-  const onLayoutRootView = useCallback(async () => {
-    if (appIsReady) {
-      // This tells the splash screen to hide immediately! If we call this after
-      // `setAppIsReady`, then we may see a blank screen while the app is
-      // loading its initial state and rendering its first pixels. So instead,
-      // we hide the splash screen once we know the root view has already
-      // performed layout.
-      await SplashScreen.hideAsync();
-    }
-  }, [appIsReady]);
-
-  if (!appIsReady) {
-    return null;
-  }
 
   return (
     <View onLayout={onLayoutRootView} style={styles.container}>
